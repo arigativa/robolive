97c62bf45952ed6c69201c8162992ebc14a54e0b
Alexander Hasselbach <alex@arigativa.ru>
2020-01-19 09:33:00 +0100

    Add default RTMP

--- system/main.tf
+++ system/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "eu-central-1"
+  # TODO revoke these token before public release
   access_key = "AKIA2HISUM5U7HWXCIHO"
   secret_key = "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"
 }
