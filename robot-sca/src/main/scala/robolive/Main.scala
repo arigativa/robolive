c82c9530c668efacd323b43c30c9aed0197dc90a
Rob <gubinrobert@gmail.com>
2020-04-15 19:39:00 +0200

    message to signalling sent

--- robot-sca/src/main/scala/robolive/Main.scala
+++ robot-sca/src/main/scala/robolive/Main.scala
@@ -1,24 +1,36 @@
 package robolive
-import org.freedesktop.gstreamer.{Element, ElementFactory, Gst, Pipeline, State}
 import org.freedesktop.gstreamer.webrtc.{WebRTCBin, WebRTCSessionDescription}
-import zio.{UIO, ZIO}
+import org.freedesktop.gstreamer._
+import sttp.client.ws.{WebSocket, WebSocketEvent}
+import sttp.model.ws.WebSocketFrame
+import zio.console.Console
+import zio.{Task, UIO, ZIO, ZManaged}
 
-final class AppLogic
+final class AppLogic(ws: WebSocket[Task])
     extends WebRTCBin.ON_NEGOTIATION_NEEDED with WebRTCBin.ON_ICE_CANDIDATE
     with WebRTCBin.CREATE_OFFER {
   override def onNegotiationNeeded(elem: Element): Unit = {
     println(s"Negotiation needed: ${elem.getName}")
+    val sendReceive = elem.asInstanceOf[WebRTCBin]
+    sendReceive.createOffer(this)
   }
 
   override def onIceCandidate(sdpMLineIndex: Int, candidate: String): Unit = {
     println(s"Ice candidate: $sdpMLineIndex $candidate")
   }
   override def onOfferCreated(offer: WebRTCSessionDescription): Unit = {
-    println(offer.getSDPMessage.toString)
+    zio.Runtime.global.unsafeRun {
+      ws.send(
+        WebSocketFrame
+          .text(
+            s"""{"sdp": {"type": "offer", "sdp": ${offer.getSDPMessage.toString}}}""".stripMargin
+          ),
+      )
+    }
   }
 }
 
-object Main extends App {
+object Application {
   private val PipelineDescription =
     """webrtcbin name=sendrecv bundle-policy=max-bundle stun-server=stun://stun.l.google.com:19302
       | videotestsrc is-live=true pattern=ball ! videoconvert ! queue ! vp8enc deadline=1 ! rtpvp8pay !
@@ -26,40 +38,85 @@ object Main extends App {
       | audiotestsrc is-live=true wave=red-noise ! audioconvert ! audioresample ! queue ! opusenc ! rtpopuspay !
       | queue ! application/x-rtp,media=audio,encoding-name=OPUS,payload=96 ! sendrecv.""".stripMargin
 
-  Gst.init("robolive-robot")
+  case object GSTInit
 
-  val process = Gst.parseBinFromDescription(PipelineDescription, false)
+  def gstInit(name: String): ZManaged[Any, Throwable, GSTInit.type] =
+    ZManaged.make(Task {
+      Gst.init(name, "--gst-disable-segtrap")
+      GSTInit
+    })(_ => UIO(Gst.deinit()))
 
-  val pipeline = new Pipeline("robolive-robot-pipeline")
-  val added = pipeline.add(process)
-  assert(added, "Can not add bin to pipeline")
+  def parseDescription(implicit ev: GSTInit.type): ZManaged[Console, Throwable, Pipeline] =
+    ZManaged.make(Task {
+      val process = Gst.parseBinFromDescription(PipelineDescription, false)
+      val pipeline = new Pipeline("robolive-robot-pipeline")
+      val added = pipeline.add(process)
+      assert(added, "Can not add bin to pipeline")
+      pipeline
+    })(pipeline =>
+      UIO {
+        pipeline.setState(State.NULL)
+        pipeline.dispose()
+      }
+    )
 
-  val sendrcv = pipeline.getElementByName("sendrecv").asInstanceOf[WebRTCBin]
-  assert(sendrcv != null, "Can not find sendrecv")
-  println("BEFORE READY")
-  pipeline.setState(State.READY)
+  def getWebRTCBin(pipeline: Pipeline, name: String)(
+    implicit ev: GSTInit.type
+  ): ZManaged[Console, Throwable, WebRTCBin] =
+    ZManaged.make(Task {
+      val sendrcv = pipeline.getElementByName(name).asInstanceOf[WebRTCBin]
+      assert(sendrcv != null, "Can not find sendrecv")
+      sendrcv
+    })(webrtc => UIO(webrtc.dispose()))
 
-  val logic = new AppLogic
-  sendrcv.connect(logic: WebRTCBin.ON_NEGOTIATION_NEEDED)
-  sendrcv.connect(logic: WebRTCBin.ON_ICE_CANDIDATE)
-  sendrcv.createOffer(logic)
+  def init(ws: WebSocket[Task]): ZManaged[Console, Throwable, StateChangeReturn] = {
+    gstInit("robolive-robot").flatMap { implicit token =>
+      for {
+        pipeline <- parseDescription
+        sendReceive <- getWebRTCBin(pipeline, "sendrecv")
+      } yield {
+        println("BEFORE READY")
+        pipeline.setState(State.READY)
 
-  println("BEFORE PLAYING")
-  pipeline.setState(State.PLAYING)
+        val logic = new AppLogic(ws)
+        sendReceive.connect(logic: WebRTCBin.ON_NEGOTIATION_NEEDED)
+        sendReceive.connect(logic: WebRTCBin.ON_ICE_CANDIDATE)
+        sendReceive.createOffer(logic)
 
-  Thread.sleep(10000)
+        pipeline.setState(State.PLAYING)
+      }
+    }
+  }
 }
 
 // https://sttp.softwaremill.com/en/latest/websockets.html
 // https://sttp.softwaremill.com/en/latest/backends/zio.html
-object Ws extends zio.App {
+object Main extends zio.App {
   import sttp.client._
-  import sttp.client.ws.{WebSocket, WebSocketResponse}
+  import sttp.client.asynchttpclient.zio.{AsyncHttpClientZioBackend, ZioWebSocketHandler}
+  import sttp.client.ws.WebSocket
   import sttp.model.ws.WebSocketFrame
-  import sttp.client.asynchttpclient.zio.ZioWebSocketHandler
-  import sttp.client.asynchttpclient.WebSocketHandler
-  import sttp.client.asynchttpclient.zio.AsyncHttpClientZioBackend
   import zio.Task
+
+  def handleMessage(
+    message: Either[WebSocketEvent, String],
+    ws: WebSocket[Task]
+  ): ZIO[Console, Throwable, Unit] = {
+    println(s"RECEIVED: $message")
+    message match {
+      case Right("READY") =>
+        Application.init(ws).use {
+          case StateChangeReturn.SUCCESS =>
+            UIO(Thread.sleep(60000))
+          case other =>
+            zio.console
+              .putStr(other.toString)
+              .flatMap(_ => UIO(Thread.sleep(60000)))
+        }
+      case Right(message) => UIO(println(message))
+      case Left(event) => ws.close
+    }
+  }
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
     AsyncHttpClientZioBackend
@@ -72,11 +129,13 @@ object Ws extends zio.App {
             val ws: WebSocket[Task] = r.result
             for {
               _ <- ws.send(WebSocketFrame.text("ROBOT"))
-              response <- ws.receiveText().tap(r => UIO(println(s"RECEIVED: $r"))).forever
-              _ <- ws.close
+              _ <- ws.receiveText().flatMap(handleMessage(_, ws)).forever
             } yield ()
           }
       }
-      .fold(_ => 1, _ => 0)
+      .fold(error => {
+        println(error)
+        1
+      }, _ => 0)
   }
 }
