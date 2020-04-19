0db4a3e48065e26d6c971fdcdc428b31ab76721a
Rob <gubinrobert@gmail.com>
2020-04-19 23:32:56 +0200

    sdp here and there, message queues

--- robot-sca/build.sbt
+++ robot-sca/build.sbt
@@ -6,11 +6,16 @@ scalaVersion := "2.13.1"
 
 val zioVersion = "1.0.0-RC18-2"
 val sttpVersion = "2.0.7"
+val circeVersion = "0.12.3"
+
 libraryDependencies ++= Seq(
   "org.freedesktop.gstreamer" % "gst1-java-core" % "1.1.0",
   "com.softwaremill.sttp.client" %% "core" % sttpVersion,
   "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % sttpVersion,
   "dev.zio" %% "zio" % zioVersion,
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion,
   "dev.zio" %% "zio-test" % zioVersion % Test,
   "dev.zio" %% "zio-test-sbt" % zioVersion % Test,
 )
--- robot-sca/src/main/scala/robolive/Main.scala
+++ robot-sca/src/main/scala/robolive/Main.scala
@@ -1,14 +1,20 @@
 package robolive
+
+import io.circe
 import org.freedesktop.gstreamer.webrtc.{WebRTCBin, WebRTCSessionDescription}
 import org.freedesktop.gstreamer._
-import sttp.client.ws.{WebSocket, WebSocketEvent}
-import sttp.model.ws.WebSocketFrame
 import zio.console.Console
-import zio.{Task, UIO, ZIO, ZManaged}
+import zio.{Ref, Task, UIO, ZIO, ZManaged, ZQueue}
 
-final class AppLogic(ws: WebSocket[Task])
-    extends WebRTCBin.ON_NEGOTIATION_NEEDED with WebRTCBin.ON_ICE_CANDIDATE
-    with WebRTCBin.CREATE_OFFER {
+final class AppLogic(
+  incomingMessages: zio.Queue[Models.Message],
+  outgoingMessages: zio.Queue[Models.Message],
+  quit: zio.Promise[Nothing, Unit],
+) extends WebRTCBin.ON_NEGOTIATION_NEEDED with WebRTCBin.ON_ICE_CANDIDATE
+    with WebRTCBin.CREATE_OFFER with Bus.EOS with Bus.ERROR {
+
+  private def run[T](task: Task[T]) = zio.Runtime.global.unsafeRunAsync_(task)
+
   override def onNegotiationNeeded(elem: Element): Unit = {
     println(s"Negotiation needed: ${elem.getName}")
     val sendReceive = elem.asInstanceOf[WebRTCBin]
@@ -18,15 +24,18 @@ final class AppLogic(ws: WebSocket[Task])
   override def onIceCandidate(sdpMLineIndex: Int, candidate: String): Unit = {
     println(s"Ice candidate: $sdpMLineIndex $candidate")
   }
+
   override def onOfferCreated(offer: WebRTCSessionDescription): Unit = {
-    zio.Runtime.global.unsafeRun {
-      ws.send(
-        WebSocketFrame
-          .text(
-            s"""{"sdp": {"type": "offer", "sdp": ${offer.getSDPMessage.toString}}}""".stripMargin
-          ),
-      )
+    run {
+      outgoingMessages.offer(Models.Sdp(`type` = "offer", sdp = offer.getSDPMessage.toString))
     }
+  }
+  override def endOfStream(source: GstObject): Unit = run {
+    quit.succeed(())
+  }
+
+  override def errorMessage(source: GstObject, code: Int, message: String): Unit = run {
+    quit.succeed(())
   }
 }
 
@@ -42,9 +51,15 @@ object Application {
 
   def gstInit(name: String): ZManaged[Any, Throwable, GSTInit.type] =
     ZManaged.make(Task {
-      Gst.init(name, "--gst-disable-segtrap")
+      Gst.init(name)
+      Gst.setSegTrap(false)
       GSTInit
-    })(_ => UIO(Gst.deinit()))
+    })(_ =>
+      UIO {
+        Gst.deinit()
+        Gst.quit()
+      }
+    )
 
   def parseDescription(implicit ev: GSTInit.type): ZManaged[Console, Throwable, Pipeline] =
     ZManaged.make(Task {
@@ -55,7 +70,7 @@ object Application {
       pipeline
     })(pipeline =>
       UIO {
-        pipeline.setState(State.NULL)
+        pipeline.stop()
         pipeline.dispose()
       }
     )
@@ -69,21 +84,35 @@ object Application {
       sendrcv
     })(webrtc => UIO(webrtc.dispose()))
 
-  def init(ws: WebSocket[Task]): ZManaged[Console, Throwable, StateChangeReturn] = {
+  def init(
+    incomingMessages: zio.Queue[Models.Message],
+    outgoingMessages: zio.Queue[Models.Message]
+  ): ZManaged[Console, Throwable, zio.Promise[Nothing, Unit]] = {
     gstInit("robolive-robot").flatMap { implicit token =>
       for {
+        quit <- zio.Promise.make[Nothing, Unit].toManaged_
         pipeline <- parseDescription
         sendReceive <- getWebRTCBin(pipeline, "sendrecv")
+        stateChange <- Task {
+          println("BEFORE READY")
+          pipeline.setState(State.READY)
+
+          val logic = new AppLogic(incomingMessages, outgoingMessages, quit)
+          sendReceive.connect(logic: WebRTCBin.ON_NEGOTIATION_NEEDED)
+          sendReceive.connect(logic: WebRTCBin.ON_ICE_CANDIDATE)
+          sendReceive.createOffer(logic)
+
+          pipeline.setState(State.PLAYING)
+        }.toManaged_
+        _ <- ZIO
+          .when(stateChange != StateChangeReturn.SUCCESS) {
+            zio.console
+              .putStrLn(s"Wrong pipeline state change: $stateChange")
+              .zipRight(quit.succeed(()))
+          }
+          .toManaged_
       } yield {
-        println("BEFORE READY")
-        pipeline.setState(State.READY)
-
-        val logic = new AppLogic(ws)
-        sendReceive.connect(logic: WebRTCBin.ON_NEGOTIATION_NEEDED)
-        sendReceive.connect(logic: WebRTCBin.ON_ICE_CANDIDATE)
-        sendReceive.createOffer(logic)
-
-        pipeline.setState(State.PLAYING)
+        quit
       }
     }
   }
@@ -98,26 +127,6 @@ object Main extends zio.App {
   import sttp.model.ws.WebSocketFrame
   import zio.Task
 
-  def handleMessage(
-    message: Either[WebSocketEvent, String],
-    ws: WebSocket[Task]
-  ): ZIO[Console, Throwable, Unit] = {
-    println(s"RECEIVED: $message")
-    message match {
-      case Right("READY") =>
-        Application.init(ws).use {
-          case StateChangeReturn.SUCCESS =>
-            UIO(Thread.sleep(60000))
-          case other =>
-            zio.console
-              .putStr(other.toString)
-              .flatMap(_ => UIO(Thread.sleep(60000)))
-        }
-      case Right(message) => UIO(println(message))
-      case Left(event) => ws.close
-    }
-  }
-
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
     AsyncHttpClientZioBackend
       .managed()
@@ -128,8 +137,33 @@ object Main extends zio.App {
           .flatMap { r =>
             val ws: WebSocket[Task] = r.result
             for {
+              incomingMessages <- ZQueue.unbounded[Models.Message]
+              outgoingMessages <- ZQueue.unbounded[Models.Message]
               _ <- ws.send(WebSocketFrame.text("ROBOT"))
-              _ <- ws.receiveText().flatMap(handleMessage(_, ws)).forever
+              _ <- ws.receiveText().flatMap(m => zio.console.putStrLn(s"$m"))
+              _ <- ws.receiveText().flatMap(m => zio.console.putStrLn(s"$m"))
+              _ <- ws
+                .receiveText()
+                .flatMap {
+                  case Left(error) =>
+                    zio.console.putStrLn(s"Error: $error").zipRight(ws.close)
+                  case Right(message) =>
+                    if (message != "READY") {
+                      Models.Message.fromWire(message) match {
+                        case Left(error) =>
+                          zio.console.putStr(s"Error while decoding message: $error")
+                        case Right(message) => incomingMessages.offer(message).map(_ => ())
+                      }
+                    } else {
+                      zio.console.putStrLn("ANOTHER READY MESSAGE") // TODO: browser sends read message each time when it tries to connect
+                    }
+                }
+                .forever
+                .fork
+              _ <- outgoingMessages.take.flatMap { message =>
+                ws.send(WebSocketFrame.text(Models.Message.toWire(message)))
+              }.fork
+              _ <- Application.init(incomingMessages, outgoingMessages).use(_.await)
             } yield ()
           }
       }
@@ -137,5 +171,36 @@ object Main extends zio.App {
         println(error)
         1
       }, _ => 0)
+  }
+}
+
+object Models {
+  import io.circe.{Decoder, Encoder}
+  import io.circe.generic.semiauto._
+
+  sealed trait Message // TODO: lowercase discriminator
+  object Message {
+    import io.circe.syntax._
+    import io.circe.parser._
+
+    def toWire(message: Message): String = message.asJson.noSpaces
+
+    def fromWire(message: String): Either[circe.Error, Message] =
+      parse(message).flatMap(_.as[Message])
+
+    implicit val encoder: Encoder[Message] = deriveEncoder
+    implicit val decoder: Decoder[Message] = deriveDecoder
+  }
+
+  final case class Sdp(`type`: String, sdp: String) extends Message
+  object Sdp {
+    implicit val encoder: Encoder[Sdp] = deriveEncoder
+    implicit val decoder: Decoder[Sdp] = deriveDecoder
+  }
+
+  final case class Ice(candidate: String, sdpMLineIndex: Int) extends Message
+  object Ice {
+    implicit val encoder: Encoder[Ice] = deriveEncoder
+    implicit val decoder: Decoder[Ice] = deriveDecoder
   }
 }
