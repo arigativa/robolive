c8f672b1c6a6094809908cadbeeefe7d3b301993
Anton Ovechkin <owanturist@gmail.com>
2020-06-14 19:45:07 +0700

    enhancement/19 Keep only remote video

--- client/src/JsSIP/index.ts
+++ client/src/JsSIP/index.ts
@@ -2,9 +2,11 @@ import { html, define, Hybrids } from 'hybrids';
 import { WebSocketInterface, UA } from 'jssip/lib/JsSIP';
 import { UAConfiguration } from 'jssip/lib/UA';
 
+const noop = () => {
+    // do nothing
+};
 
 // W E B   C O M P O N E N T
-
 
 interface WebRtcRemoteViewElement extends HTMLVideoElement {
     init: never;
@@ -12,17 +14,17 @@ interface WebRtcRemoteViewElement extends HTMLVideoElement {
     uri: string;
     with_audio: boolean;
     with_video: boolean;
+    render(): HTMLElement;
 }
 
 const WebRtcRemoteViewElement: Hybrids<WebRtcRemoteViewElement> = {
     init: {
         connect: host => {
             const root = host.render();
-            const localVideo = root.querySelector('.local-video');
-            const remoteVideo = root.querySelector('.remote-video');
+            const remoteVideo = root.querySelector<HTMLVideoElement>('video');
 
-            if (localVideo == null || remoteVideo == null) {
-                return
+            if (remoteVideo == null) {
+                return noop;
             }
 
             const session = host.user_agent.call(
@@ -39,12 +41,6 @@ const WebRtcRemoteViewElement: Hybrids<WebRtcRemoteViewElement> = {
             );
 
             session.on('confirmed', () => {
-                const localStreams = session.connection.getLocalStreams();
-
-                if (localStreams.length > 0) {
-                    localVideo.srcObject = localStreams[ 0 ].clone()
-                }
-
                 const remoteStreams = session.connection.getRemoteStreams();
 
                 if (remoteStreams.length > 0) {
@@ -52,96 +48,21 @@ const WebRtcRemoteViewElement: Hybrids<WebRtcRemoteViewElement> = {
                 }
             });
 
-            session.on('peerconnection', event => {
-                console.log('peerconnection', event);
-            })
-            session.on('connecting', event => {
-                console.log('connecting', event);
-            })
-            session.on('sending', event => {
-                console.log('sending', event);
-            })
-            session.on('progress', event => {
-                console.log('progress', event);
-            })
-            session.on('accepted', event => {
-                console.log('accepted', event);
-            })
-            session.on('confirmed', event => {
-                console.log('confirmed', event);
-            })
-            session.on('ended', event => {
-                console.log('ended', event);
-            })
-            session.on('failed', event => {
-                console.log('failed', event);
-            })
-            session.on('newDTMF', event => {
-                console.log('newDTMF', event);
-            })
-            session.on('newInfo', event => {
-                console.log('newInfo', event);
-            })
-            session.on('hold', event => {
-                console.log('hold', event);
-            })
-            session.on('unhold', event => {
-                console.log('unhold', event);
-            })
-            session.on('muted', event => {
-                console.log('muted', event);
-            })
-            session.on('unmuted', event => {
-                console.log('unmuted', event);
-            })
-            session.on('reinvite', event => {
-                console.log('reinvite', event);
-            })
-            session.on('update', event => {
-                console.log('update', event);
-            })
-            session.on('refer', event => {
-                console.log('refer', event);
-            })
-            session.on('replaces', event => {
-                console.log('replaces', event);
-            })
-            session.on('sdp', event => {
-                console.log('sdp', event);
-            })
-            session.on('icecandidate', event => {
-                console.log('icecandidate', event);
-            })
-            session.on('getusermediafailed', event => {
-                console.log('getusermediafailed', event);
-            })
-            session.on('peerconnection:createofferfailed', event => {
-                console.log('peerconnection:createofferfailed', event);
-            })
-            session.on('peerconnection:createanswerfailed', event => {
-                console.log('peerconnection:createanswerfailed', event);
-            })
-            session.on('peerconnection:setlocaldescriptionfailed', event => {
-                console.log('peerconnection:setlocaldescriptionfailed', event);
-            })
-            session.on('peerconnection:setremotedescriptionfailed', event => {
-                console.log('peerconnection:setremotedescriptionfailed', event);
-            })
-
             session.connection.addEventListener('addstream', event => {
                 const { stream } = event as MediaStreamEvent
 
                 remoteVideo.srcObject = stream
             });
+
+            return () => {
+                session.terminate();
+            };
         }
     },
 
-    render(host) {
-        host.init;
-
+    render() {
         return html`
-            <video class="local-video" muted autoplay></video>
-            <video class="remote-video" autoplay></video>
+            <video autoplay></video>
         `;
     }
 };
