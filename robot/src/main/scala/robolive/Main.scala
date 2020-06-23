a1a8f53808d39eff7606f15a8740c848015dc015
Rob <gubinrobert@gmail.com>
2020-06-23 11:34:40 +0200

    read input stream

--- robot/src/main/scala/robolive/Main.scala
+++ robot/src/main/scala/robolive/Main.scala
@@ -72,6 +72,8 @@ object Main extends App {
     new RuntimeException(s"Wrong pipeline state change: $stateChange")
   }
   val bin = WebRTCBinManaged(pipeline, "sendrecv")
+  bin.onPadAdded(onIncomingStream)
+
   val controller = new WebRTCController(bin)
   val sipEventsHandler = new SIPCallEventHandler(controller)
   val sipClient = new SipClient(sipEventsHandler)
@@ -85,6 +87,53 @@ object Main extends App {
     s"""webrtcbin name=sendrecv bundle-policy=max-bundle stun-server=stun://stun.l.google.com:19302
        | $videoSrc ! queue ! vp8enc deadline=1 ! rtpvp8pay pt=120 !
        | queue ! application/x-rtp,media=video,encoding-name=VP8,payload=120 ! sendrecv.""".stripMargin
+  }
+
+  private def onIncomingStream(pad: Pad): Unit = {
+    if (pad.getDirection != PadDirection.SRC) {
+      println("Pad direction incorrect")
+    } else {
+      val decodebin = ElementFactory.make("decodebin", "inDecodeBin")
+      decodebin.connect(new Element.PAD_ADDED {
+        override def padAdded(
+          element: Element,
+          pad: Pad
+        ): Unit = onIncomingDecodebinStream(pad)
+      })
+      pipeline.add(decodebin)
+      decodebin.syncStateWithParent()
+      bin.link(decodebin)
+    }
+  }
+
+  private def onIncomingDecodebinStream(pad: Pad) = {
+    if (!pad.hasCurrentCaps) {
+      println(s"Pad ${pad.getName} has no caps")
+    } else {
+      val caps = pad.getCurrentCaps
+      if (caps.size() <= 0) {
+        println("Caps size is 0")
+      } else {
+        val structure = caps.getStructure(0)
+        val name = structure.getName()
+        if (name.startsWith("video")) {
+          val queue = ElementFactory.make("queue", "incomingBuffer")
+          val convert = ElementFactory.make("videoconvert", "videoconvert")
+          val sink = ElementFactory.make("autovideosink", "autovideosink")
+          pipeline.add(queue)
+          pipeline.add(convert)
+          pipeline.add(sink)
+          queue.syncStateWithParent()
+          convert.syncStateWithParent()
+          sink.syncStateWithParent()
+          pad.link(queue.getStaticPad("sink"))
+          queue.link(convert)
+          convert.link(sink)
+        } else {
+          println("No supported receiver")
+        }
+      }
+    }
   }
 
 }
