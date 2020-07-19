f22ea09a737674904fe8a40449fa8c354782d527
Rob <gubinrobert@gmail.com>
2020-07-19 15:44:28 +0200

    logging improved

--- robot/src/main/scala/robolive/Main.scala
+++ robot/src/main/scala/robolive/Main.scala
@@ -3,196 +3,17 @@ package robolive
 import java.util.concurrent.CountDownLatch
 
 import org.freedesktop.gstreamer._
-import org.mjsip.sip.call.ExtendedCall
-import robolive.gstreamer.{GstManaged, PipelineManaged, WebRTCBinManaged}
-import sdp.SdpMessage.RawValueAttribute
-import sdp.{Attributes, SdpMessage}
+import org.slf4j.LoggerFactory
+import robolive.gstreamer.GstManaged
 
-import scala.concurrent.{ExecutionContext, Future}
-
-sealed trait WebRTCControllerPlayState
-
-object WebRTCControllerPlayState {
-  case object Failure extends WebRTCControllerPlayState
-  case object Wait extends WebRTCControllerPlayState
-  case object Busy extends WebRTCControllerPlayState
-}
-
-object WebRTCController {
-  def pipelineDescription(videoSrc: String, rtcType: Int, stunServerUrl: String): String = {
-    s"""webrtcbin name=sendrecv bundle-policy=max-bundle stun-server=$stunServerUrl
-       | $videoSrc ! queue ! vp8enc deadline=1 ! rtpvp8pay pt=$rtcType !
-       | queue ! application/x-rtp,media=video,encoding-name=VP8,payload=$rtcType ! sendrecv.""".stripMargin
-  }
-}
-
-final class WebRTCController(videoSrc: String)(implicit gst: GstManaged.GSTInit.type) {
-  private var pipeline: Pipeline = _
-  private var webRTCBin: WebRTCBinManaged = _
-  @volatile private var state: WebRTCControllerPlayState = WebRTCControllerPlayState.Wait
-
-  private def start(rtcType: Int): StateChangeReturn = synchronized {
-    val pipelineDescription = WebRTCController.pipelineDescription(
-      videoSrc = videoSrc,
-      rtcType = rtcType,
-      stunServerUrl = "stun://stun.l.google.com:19302"
-    )
-
-    pipeline = PipelineManaged(
-      name = "robolive-robot-pipeline",
-      description = pipelineDescription,
-    )
-    pipeline.ready()
-    val bus = pipeline.getBus
-
-    val eosHandler: Bus.EOS =
-      (source: GstObject) => println(s"EOS OCCURRED: ${source.getName}")
-
-    val errorHandler: Bus.ERROR = (source: GstObject, code: Int, message: String) =>
-      println(s"ERROR OCCURRED: ${source.getName} $code $message")
-
-    bus.connect(eosHandler)
-    bus.connect(errorHandler)
-
-    val stateChange = pipeline.play()
-    if (stateChange == StateChangeReturn.SUCCESS) {
-      webRTCBin = WebRTCBinManaged(pipeline, "sendrecv")
-      webRTCBin.onPadAdded(onIncomingStream)
-    } else {
-      pipeline = null
-      webRTCBin = null
-    }
-    stateChange
-  }
-
-  def dispose(): Unit = synchronized {
-    pipeline.stop()
-    pipeline.dispose()
-    webRTCBin = null
-    pipeline = null
-    println("STATE: WAIT")
-    state = WebRTCControllerPlayState.Wait
-  }
-
-  private def onIncomingStream(pad: Pad): Unit = {
-    if (pad.getDirection != PadDirection.SRC) {
-      println("Pad direction incorrect")
-    } else {
-      val decodebin = ElementFactory.make("decodebin", "inDecodeBin")
-      decodebin.connect(new Element.PAD_ADDED {
-        override def padAdded(
-          element: Element,
-          pad: Pad
-        ): Unit = onIncomingDecodebinStream(pad)
-      })
-      pipeline.add(decodebin)
-      decodebin.syncStateWithParent()
-      webRTCBin.link(decodebin)
-    }
-  }
-
-  private def onIncomingDecodebinStream(pad: Pad) = {
-    if (!pad.hasCurrentCaps) {
-      println(s"Pad ${pad.getName} has no caps")
-    } else {
-      val caps = pad.getCurrentCaps
-      if (caps.size() <= 0) {
-        println("Caps size is 0")
-      } else {
-        val structure = caps.getStructure(0)
-        val name = structure.getName()
-        if (name.startsWith("video")) {
-          val queue = ElementFactory.make("queue", "incomingBuffer")
-          val convert = ElementFactory.make("videoconvert", "videoconvert")
-          val sink = ElementFactory.make("autovideosink", "autovideosink")
-          pipeline.add(queue)
-          pipeline.add(convert)
-          pipeline.add(sink)
-          queue.syncStateWithParent()
-          convert.syncStateWithParent()
-          sink.syncStateWithParent()
-          pad.link(queue.getStaticPad("sink"))
-          queue.link(convert)
-          convert.link(sink)
-        } else {
-          println("No supported receiver")
-        }
-      }
-    }
-  }
-
-  private def getRtpType(sdp: SdpMessage): Either[Seq[String], Int] = {
-    for {
-      video <- sdp.getMedia("video").toRight(Seq("Video media not found"))
-      rtpMaps <- video.getAttributes[Attributes.RtpMap]("rtpmap")
-      attr <- rtpMaps.find(_.encodingName == "VP8").toRight(Seq("VP8 codec not found in RtpMap"))
-    } yield {
-      attr.payloadType
-    }
-  }
-
-  private def fixSdpForChrome(sdp: String) = {
-    sdp + System.lineSeparator()
-  }
-
-  def makeCall(call: ExtendedCall, remoteSdp: SdpMessage)(
-    implicit ec: ExecutionContext
-  ): Future[Unit] = synchronized {
-    state match {
-      case WebRTCControllerPlayState.Wait | WebRTCControllerPlayState.Failure =>
-        println("STATE: WAIT | FAILURE STATE")
-        getRtpType(remoteSdp) match {
-          case Right(rtpType) =>
-            println(s"STATE: RTP TYPE EXTRACTED $rtpType")
-            if (start(rtpType) == StateChangeReturn.SUCCESS) {
-              state = WebRTCControllerPlayState.Busy
-
-              call.ring()
-
-              webRTCBin.setRemoteOffer(remoteSdp)
-              (for {
-                localIceCandidates <- webRTCBin.fetchIceCandidates()
-                answer <- webRTCBin.createAnswer()
-              } yield {
-                webRTCBin.setLocalAnswer(answer)
-
-                val mediasWithCandidates = localIceCandidates.groupBy(_.sdpMLineIndex).map {
-                  case (mIndex, candidates) =>
-                    val attrs = candidates.map(c => RawValueAttribute("candidate", c.candidate))
-                    answer.media(mIndex).attributesAdded(attrs)
-                }
-
-                val answerWithCandidates = answer.copy(media = mediasWithCandidates.toSeq)
-
-                println(s"ANSWER PREPARED:\n ${answerWithCandidates.toSdpString}")
-
-                val sdpString = fixSdpForChrome(answerWithCandidates.toSdpString)
-                call.accept(sdpString)
-              }).recover {
-                case error =>
-                  error.printStackTrace()
-                  state = WebRTCControllerPlayState.Failure
-                  Future.successful(call.refuse())
-              }
-            } else {
-              state = WebRTCControllerPlayState.Failure
-              println("STATE: FAILED TO START")
-              Future.successful(call.refuse())
-            }
-          case Left(errors) =>
-            state = WebRTCControllerPlayState.Failure
-            println(s"STATE: FAILED TO EXTRACT RTPTYPE: ERRORS: ${errors.mkString(", ")}")
-            Future.successful(call.refuse())
-        }
-      case WebRTCControllerPlayState.Busy =>
-        println("STATE: BUSY")
-        Future.successful(call.hangup())
-    }
-
-  }
-}
+import scala.concurrent.ExecutionContext
 
 object Main extends App {
+  private val logger = LoggerFactory.getLogger(getClass.getName)
+
+  val robotName = "robomachine"
+
+  logger.info(s"Starting robot: $robotName")
 
   implicit val ec: ExecutionContext = ExecutionContext.global
 
@@ -204,15 +25,16 @@ object Main extends App {
     val defaultVideoSrcPipeline = "videotestsrc is-live=true pattern=ball ! videoconvert"
     getEnv("VIDEO_SRC", defaultVideoSrcPipeline)
   }
+
   val latch = new CountDownLatch(1)
 
-  implicit val gstInit: GstManaged.GSTInit.type = GstManaged("robolive-robot", new Version(1, 14))
+  implicit val gstInit: GstManaged.GSTInit.type = GstManaged(robotName, new Version(1, 14))
 
   val controller = new WebRTCController(videoSrc)
   val sipEventsHandler = new SIPCallEventHandler(controller)
   val sipConfig = SipConfig(
     registrarUri = "localhost:9031",
-    name = "robomachine",
+    name = robotName,
     protocol = "tcp",
   )
   val sipClient = new SipClient(sipEventsHandler, sipConfig)
---
+++ robot/src/main/scala/robolive/WebRTCController.scala
@@ -0,0 +1,211 @@
+package robolive
+
+import org.freedesktop.gstreamer._
+import org.mjsip.sip.call.ExtendedCall
+import org.slf4j.LoggerFactory
+import robolive.gstreamer.{GstManaged, PipelineManaged, WebRTCBinManaged}
+import sdp.SdpMessage.RawValueAttribute
+import sdp.{Attributes, SdpMessage}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+final class WebRTCController(videoSrc: String)(implicit gst: GstManaged.GSTInit.type) {
+  import WebRTCController._
+
+  private val logger = LoggerFactory.getLogger(getClass.getName)
+
+  private var pipeline: Pipeline = _
+  private var webRTCBin: WebRTCBinManaged = _
+  @volatile private var state: WebRTCControllerPlayState = WebRTCControllerPlayState.Wait
+
+  private def start(rtcType: Int): StateChangeReturn = synchronized {
+    val pipelineDescription = WebRTCController.pipelineDescription(
+      videoSrc = videoSrc,
+      rtcType = rtcType,
+      stunServerUrl = "stun://stun.l.google.com:19302"
+    )
+
+    pipeline = PipelineManaged(
+      name = "robolive-robot-pipeline",
+      description = pipelineDescription,
+    )
+    pipeline.ready()
+    val bus = pipeline.getBus
+
+    val eosHandler: Bus.EOS =
+      (source: GstObject) => logger.info(s"EOS ${source.getName}")
+
+    val errorHandler: Bus.ERROR = (source: GstObject, code: Int, message: String) =>
+      logger.error(s"Error ${source.getName}: $code $message")
+
+    bus.connect(eosHandler)
+    bus.connect(errorHandler)
+
+    val stateChange = pipeline.play()
+    if (stateChange == StateChangeReturn.SUCCESS) {
+      webRTCBin = WebRTCBinManaged(pipeline, "sendrecv")
+      webRTCBin.onPadAdded(onIncomingStream)
+    } else {
+      pipeline = null
+      webRTCBin = null
+    }
+    stateChange
+  }
+
+  def dispose(): Unit = synchronized {
+    pipeline.stop()
+    pipeline.dispose()
+    webRTCBin = null
+    pipeline = null
+    logger.debug("State transition to 'WAIT'")
+    state = WebRTCControllerPlayState.Wait
+  }
+
+  private def onIncomingStream(pad: Pad): Unit = {
+    if (pad.getDirection != PadDirection.SRC) {
+      logger.error("Error incoming stream: pad direction incorrect")
+    } else {
+      val decodebin = ElementFactory.make("decodebin", "inDecodeBin")
+      decodebin.connect(new Element.PAD_ADDED {
+        override def padAdded(
+          element: Element,
+          pad: Pad
+        ): Unit = onIncomingDecodebinStream(pad)
+      })
+      pipeline.add(decodebin)
+      decodebin.syncStateWithParent()
+      webRTCBin.link(decodebin)
+    }
+  }
+
+  private def onIncomingDecodebinStream(pad: Pad) = {
+    if (!pad.hasCurrentCaps) {
+      logger.error(s"Error incoming stream ${pad.getName}: pad has no caps")
+    } else {
+      val caps = pad.getCurrentCaps
+      if (caps.size() <= 0) {
+        logger.error(s"Error incoming stream ${pad.getName}: pad has no caps")
+      } else {
+        val structure = caps.getStructure(0)
+        val name = structure.getName()
+        val receiverName = "video"
+        if (name.startsWith(receiverName)) {
+          val queue = ElementFactory.make("queue", "incomingBuffer")
+          val convert = ElementFactory.make("videoconvert", "videoconvert")
+          val sink = ElementFactory.make("autovideosink", "autovideosink")
+          pipeline.add(queue)
+          pipeline.add(convert)
+          pipeline.add(sink)
+          queue.syncStateWithParent()
+          convert.syncStateWithParent()
+          sink.syncStateWithParent()
+          pad.link(queue.getStaticPad("sink"))
+          queue.link(convert)
+          convert.link(sink)
+        } else {
+          logger.error(
+            s"Error incoming stream ${pad.getName}: no supported $receiverName receiver, found: $name"
+          )
+        }
+      }
+    }
+  }
+
+  private def getRtpTypeForVP8Media(sdp: SdpMessage): Either[Seq[String], Int] = {
+    for {
+      video <- sdp.getMedia("video").toRight(Seq("Video media not found"))
+      rtpMaps <- video.getAttributes[Attributes.RtpMap]("rtpmap")
+      attr <- rtpMaps.find(_.encodingName == "VP8").toRight(Seq("VP8 codec not found in RtpMap"))
+    } yield {
+      attr.payloadType
+    }
+  }
+
+  private def fixSdpForChrome(sdp: String) = {
+    sdp + System.lineSeparator()
+  }
+
+  def makeCall(call: ExtendedCall, remoteSdp: SdpMessage)(
+    implicit ec: ExecutionContext
+  ): Future[Unit] = synchronized {
+    state match {
+      case WebRTCControllerPlayState.Wait | WebRTCControllerPlayState.Failure =>
+        logger.info("Processing incoming call")
+
+        getRtpTypeForVP8Media(remoteSdp) match {
+          case Right(rtpType) =>
+            logger.debug(s"Extracted rtpType: $rtpType")
+
+            if (start(rtpType) == StateChangeReturn.SUCCESS) {
+              state = WebRTCControllerPlayState.Busy
+
+              call.ring()
+
+              webRTCBin.setRemoteOffer(remoteSdp)
+              (for {
+                localIceCandidates <- webRTCBin.fetchIceCandidates()
+                answer <- webRTCBin.createAnswer()
+              } yield {
+                webRTCBin.setLocalAnswer(answer)
+
+                val mediasWithCandidates = localIceCandidates.groupBy(_.sdpMLineIndex).map {
+                  case (mIndex, candidates) =>
+                    val attrs = candidates.map(c => RawValueAttribute("candidate", c.candidate))
+                    answer.media(mIndex).attributesAdded(attrs)
+                }
+
+                val answerWithCandidates = answer.copy(media = mediasWithCandidates.toSeq)
+                val sdpString = fixSdpForChrome(answerWithCandidates.toSdpString)
+
+                logger.debug(s"Answer is ready:\n $sdpString")
+
+                call.accept(sdpString)
+              }).recover {
+                case error =>
+                  logger.error(s"Error: fail to process call ${error.getMessage}", error)
+
+                  state = WebRTCControllerPlayState.Failure
+                  logger.debug("State transition to 'FAILURE'")
+
+                  Future.successful(call.refuse())
+              }
+            } else {
+              logger.error("Failed to start")
+
+              state = WebRTCControllerPlayState.Failure
+              logger.debug("State transition to 'FAILURE'")
+
+              Future.successful(call.refuse())
+            }
+          case Left(errors) =>
+            val errorString = errors.mkString(", ")
+            logger.error(s"Error: fail to extract rtpType from SDP $errorString")
+
+            state = WebRTCControllerPlayState.Failure
+            logger.debug("State transition to 'FAILURE'")
+
+            Future.successful(call.refuse())
+        }
+      case WebRTCControllerPlayState.Busy =>
+        logger.info("Robot is busy")
+        Future.successful(call.hangup())
+    }
+
+  }
+}
+
+object WebRTCController {
+  sealed trait WebRTCControllerPlayState
+
+  object WebRTCControllerPlayState {
+    case object Failure extends WebRTCControllerPlayState
+    case object Wait extends WebRTCControllerPlayState
+    case object Busy extends WebRTCControllerPlayState
+  }
+
+  def pipelineDescription(videoSrc: String, rtcType: Int, stunServerUrl: String): String = {
+    s"""webrtcbin name=sendrecv bundle-policy=max-bundle stun-server=$stunServerUrl
+       | $videoSrc ! queue ! vp8enc deadline=1 ! rtpvp8pay pt=$rtcType !
+       | queue ! application/x-rtp,media=video,encoding-name=VP8,payload=$rtcType ! sendrecv.""".stripMargin
+  }
+}
