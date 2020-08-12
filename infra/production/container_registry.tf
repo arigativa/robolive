5d0163fb4a249824c4c43a0f6b6c6022c155c55e
hasselbach <alex@arigativa.ru>
2020-08-12 21:10:27 +0200

    Remove container registry

--- infra/production/container_registry.tf
+++ infra/production/container_registry.tf
@@ -1,26 +1,26 @@
-resource "azurerm_container_registry" "robolive" {
-  resource_group_name = azurerm_resource_group.robolive.name
-  location            = azurerm_resource_group.robolive.location
-
-  name          = "robolive"
-  admin_enabled = true
-  sku           = "Standard"
-}
-
-resource "github_actions_secret" "container_registry_registry" {
-  repository      = var.robolive_github_repository
-  secret_name     = "DOCKER_REGISTRY"
-  plaintext_value = azurerm_container_registry.robolive.login_server
-}
-
-resource "github_actions_secret" "container_registry_username" {
-  repository      = var.robolive_github_repository
-  secret_name     = "DOCKER_USERNAME"
-  plaintext_value = azurerm_container_registry.robolive.admin_username
-}
-
-resource "github_actions_secret" "container_registry_password" {
-  repository      = var.robolive_github_repository
-  secret_name     = "DOCKER_PASSWORD"
-  plaintext_value = azurerm_container_registry.robolive.admin_password
-}
+//resource "azurerm_container_registry" "robolive" {
+//  resource_group_name = azurerm_resource_group.robolive.name
+//  location            = azurerm_resource_group.robolive.location
+//
+//  name          = "robolive"
+//  admin_enabled = true
+//  sku           = "Standard"
+//}
+//
+//resource "github_actions_secret" "container_registry_registry" {
+//  repository      = var.robolive_github_repository
+//  secret_name     = "DOCKER_REGISTRY"
+//  plaintext_value = azurerm_container_registry.robolive.login_server
+//}
+//
+//resource "github_actions_secret" "container_registry_username" {
+//  repository      = var.robolive_github_repository
+//  secret_name     = "DOCKER_USERNAME"
+//  plaintext_value = azurerm_container_registry.robolive.admin_username
+//}
+//
+//resource "github_actions_secret" "container_registry_password" {
+//  repository      = var.robolive_github_repository
+//  secret_name     = "DOCKER_PASSWORD"
+//  plaintext_value = azurerm_container_registry.robolive.admin_password
+//}
