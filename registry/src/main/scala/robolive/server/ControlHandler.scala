7d65f08150b749b658985f8880b74f880a7cf7f1
Rob <gubinrobert@gmail.com>
2020-08-26 23:01:24 +0200

    robots no more

--- registry/src/main/scala/robolive/server/ControlHandler.scala
+++ registry/src/main/scala/robolive/server/ControlHandler.scala
@@ -2,7 +2,7 @@ package robolive.server
 
 import java.util.concurrent.ConcurrentHashMap
 
-import Control.{AgentListRequest, AgentListResponse, RestartRobotRequest, RestartRobotResponse}
+import Control.{AgentListRequest, AgentListResponse, RestartAgentRequest, RestartAgentResponse}
 import Control.RegistryControlGrpc.RegistryControl
 import Control.AgentView
 import Inventory.{AgentCommand, RestartAgent}
@@ -15,7 +15,7 @@ final class ControlHandler(
     extends RegistryControl {
   import scala.jdk.CollectionConverters._
 
-  override def robotsList(request: AgentListRequest): Future[AgentListResponse] = {
+  override def agentList(request: AgentListRequest): Future[AgentListResponse] = {
     Future {
       val agents = robotTable.asScala.map {
         case (id, state) => AgentView(state.status, state.name, id)
@@ -24,13 +24,13 @@ final class ControlHandler(
     }
   }
 
-  override def restartRobot(request: RestartRobotRequest): Future[RestartRobotResponse] = {
+  override def restartAgent(request: RestartAgentRequest): Future[RestartAgentResponse] = {
     val callback = robotTable.asScala.get(request.id).map(_.sendMessageCallback)
     callback match {
       case Some(call) =>
         Future {
           call(AgentCommand(AgentCommand.Command.Restart(RestartAgent())))
-          RestartRobotResponse()
+          RestartAgentResponse()
         }
 
       case None =>
