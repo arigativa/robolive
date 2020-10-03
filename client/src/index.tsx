6f65febcb86ef2321b3101076bb4b0604c95864e
Anton Ovechkin <owanturist@gmail.com>
2020-10-03 21:33:38 +0200

    Create custom effect for store provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,17 +5,25 @@ import ReactDOM from 'react-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 import * as serviceWorker from 'serviceWorker'
-import { Provider } from 'Provider'
+import { useStore } from 'store'
 import * as App from 'App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CssBaseline>
-      <Provider init={App.init('Munich')} update={App.update} view={App.View} />
-    </CssBaseline>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const Root: React.FC = () => {
+  const [state, dispatch] = useStore({
+    init: React.useMemo(() => App.init('Munich'), []),
+    update: App.update
+  })
+
+  return (
+    <React.StrictMode>
+      <CssBaseline>
+        <App.View state={state} dispatch={dispatch} />
+      </CssBaseline>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
