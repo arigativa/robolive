33b8ca419c3e904b8bf7beeb47ddc70e2ff9a41c
Anton Ovechkin <owanturist@gmail.com>
2020-12-05 23:39:03 +0100

    enhancement/121 Fix storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const toPath = _path => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: ['../src/**/*.stories.ts*', '../src/**/*.story.ts*'],
   addons: [
@@ -5,5 +8,16 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register'
-  ]
+  ],
+  webpackFinal: async config => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@emotion/core': toPath('node_modules/@emotion/react'),
+        'emotion-theming': toPath('node_modules/@emotion/react')
+      }
+    }
+  })
 }
