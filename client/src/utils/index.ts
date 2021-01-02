f3d4cba90948c2a29b9bf13410ba785f8251fe2b
Anton Ovechkin <owanturist@gmail.com>
2021-01-02 22:41:29 +0100

    enhancement/121 Remove unused utils

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -103,59 +103,6 @@ export const match = <A extends CaseOf<string, unknown>, R>(
   return (schema as { _(): R })._()
 }
 
-/**
- * Tricky helper to handle tricky Cata default '_' case.
- * In Cata we assume that default case is always present
- * in case of current is not provided.
- *
- * @param defaultFn usually '_' pattern case
- * @param fn target optional case
- * @param args arguments for optional case
- */
-export const callOrElse = <A extends Array<unknown>, R>(
-  defaultFn: (() => R) | undefined,
-  fn: ((...args: A) => R) | undefined,
-  ...args: A
-): R => {
-  return typeof fn === 'function' ? fn(...args) : (defaultFn as () => R)()
-}
-
-const noop = (): void => {
-  /* noop */
-}
-
-/**
- * Decorator guarantees to run fn just once.
- *
- * @param fn
- */
-export const once = <A extends Array<unknown>>(
-  fn: (...args: A) => void
-): ((...args: A) => void) => {
-  let cb = fn
-
-  return (...args: A): void => {
-    cb(...args)
-    cb = noop
-  }
-}
-
-/**
- * Rounds a float number with specific precision.
- *
- * @param float
- * @param precision
- */
-export const round = (float: number, precision?: number): number => {
-  if (typeof precision === 'undefined' || precision < 1) {
-    return Math.round(float)
-  }
-
-  const pow = 10 ** precision
-
-  return Math.round(pow * float) / pow
-}
-
 export const hasWhitespaces = (input: string): boolean => {
   return /.*\s+.*/.test(input)
 }
@@ -173,19 +120,8 @@ export const range = (start: number, end?: number): Array<number> => {
   return acc
 }
 
-export const repeat = <T>(n: number, element: T): Array<T> => {
-  const N = Math.max(0, n)
-  const acc = new Array(N)
-
-  for (let index = 0; index < N; index++) {
-    acc[index] = element
-  }
-
-  return acc
-}
-
-export const every = <T>(ms: number, action: (ts: number) => T): Sub<T> =>
-  Sub.create(`int-${ms}`, action, tick => {
+export const every = <T>(ms: number, action: (ts: number) => T): Sub<T> => {
+  return Sub.create(`int-${ms}`, action, tick => {
     const intId = setInterval(() => {
       tick(Date.now())
     }, ms)
@@ -194,3 +130,4 @@ export const every = <T>(ms: number, action: (ts: number) => T): Sub<T> =>
       clearInterval(intId)
     }
   })
+}
