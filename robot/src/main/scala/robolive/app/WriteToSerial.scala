162e72c13117353abb131fe64afcd1090b605601
Rob <gubinrobert@gmail.com>
2021-02-19 23:58:30 +0100

    utily fixed

--- robot/src/main/scala/robolive/app/WriteToSerial.scala
+++ robot/src/main/scala/robolive/app/WriteToSerial.scala
@@ -2,34 +2,76 @@ package robolive.app
 
 import com.fazecast.jSerialComm.SerialPort
 import robolive.puppet.driver.SerialDriver
+import robolive.utils.Hex
 
 import scala.io.Source
 
 object WriteToSerial extends App {
 
+  private val serialPorts = SerialPort.getCommPorts
+
   args match {
-    case Array("-s", systemPortName) =>
-      SerialPort.getCommPorts.find(_.getSystemPortName.contains(systemPortName)) match {
+    case Array("-l") =>
+      serialPorts.foreach { port =>
+        println(s"""
+           |system port name:      ${port.getSystemPortName} 
+           |descriptive port name: ${port.getDescriptivePortName} 
+           |port description:      ${port.getPortDescription}
+           |""".stripMargin)
+      }
+
+    case Array("-s", systemPortName, "-c", command) =>
+      serialPorts.find(_.getSystemPortName.contains(systemPortName)) match {
         case Some(port) =>
-          port.setBaudRate(9600)
-          if (!port.openPort()) {
-            throw new RuntimeException("Can't open serial port")
-          }
-          Source.stdin.getLines.foreach { line =>
-            println(s"> $line")
-            val bytes = (line + "\n").getBytes
-            port.writeBytes(bytes, bytes.length)
-          }
-          val output = Array.fill(256)(0.toByte)
           try {
-            port.readBytes(output, 256)
+            port.setBaudRate(9600)
+            val serialDriver = SerialDriver.start(port)
+
+            val writtenCommand = serialDriver.write(command + "\n")
+            println(s"$writtenCommand < $command\n")
+
+            val response = serialDriver.readline()
+            println(s"> $response")
           } catch {
-            case er: Throwable => er.printStackTrace()
+            case e: Throwable => e.printStackTrace()
+          } finally {
+            port.closePort()
           }
-          println("=========================")
-          println(output.mkString)
         case None =>
           throw new RuntimeException("Device not found")
       }
+
+    case Array("-s", systemPortName, "-c", command, "-b", bytes, "-r", read) =>
+      serialPorts.find(_.getSystemPortName.contains(systemPortName)) match {
+        case Some(port) =>
+          try {
+            port.setBaudRate(9600)
+            val serialDriver = SerialDriver.start(port)
+
+            val writtenCommand = serialDriver.write(command + "\n")
+            println(s"$writtenCommand < $command\n")
+
+            val byteString = Hex.decodeBytes(bytes)
+            val writtenBytes = serialDriver.write(byteString)
+            println(s"$writtenBytes < ${byteString.mkString}")
+
+            val response = serialDriver.read(read.toInt)
+            println(s"> ${response.mkString}")
+          } catch {
+            case e: Throwable => e.printStackTrace()
+          } finally {
+            port.closePort()
+          }
+
+        case None =>
+          throw new RuntimeException("Device not found")
+      }
+
+    case _ => println("""Unknown options, try:
+        | -l
+        | -s ttyACM1 -c reset
+        | -s ttyACM1 -c serial:5 -b FFFFFFFFFF -r 5
+        | """.stripMargin)
+
   }
 }
