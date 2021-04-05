83e66776e52d087fb66939b684bfe7ceb0f636a7
Anton Ovechkin <owanturist@gmail.com>
2021-04-05 12:55:27 +0200

    enhancement/170 Add stories fro messages

--- client/src/Room/Room.story.tsx
+++ client/src/Room/Room.story.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { text, boolean } from '@storybook/addon-knobs'
+import { text, boolean, number } from '@storybook/addon-knobs'
 import RemoteData from 'frctl/RemoteData/Optional'
 
 import * as Room from '.'
@@ -46,31 +46,23 @@ interface CanvasElement extends HTMLCanvasElement {
   captureStream(frameRate?: number): MediaStream
 }
 
-const useFakeStream = (): null | MediaStream => {
-  const [stream, setStream] = React.useState<null | MediaStream>(null)
-
-  React.useEffect(() => {
+const useFakeStream = (): MediaStream => {
+  const [stream, cleanup] = React.useMemo(() => {
     const canvas = document.createElement('canvas') as CanvasElement
     canvas.width = 1
     canvas.height = 1
     canvas.getContext('2d')?.fillRect(0, 0, 1, 1)
 
-    setStream(canvas.captureStream(1))
-
-    return () => {
-      canvas.remove()
-    }
+    return [canvas.captureStream(1), () => canvas.remove()]
   }, [])
+
+  React.useEffect(() => cleanup, [cleanup])
 
   return stream
 }
 
 export const OnCall: React.FC = () => {
   const stream = useFakeStream()
-
-  if (!stream) {
-    return null
-  }
 
   return (
     <Room.View
@@ -86,10 +78,6 @@ export const OnCall: React.FC = () => {
 export const SendInfoText: React.FC = () => {
   const info = text('Info', 'Some useful information to send')
   const stream = useFakeStream()
-
-  if (!stream) {
-    return null
-  }
 
   return (
     <Room.View
@@ -107,16 +95,74 @@ export const Terminating: React.FC = () => {
   const terminating = boolean('Terminating', true)
   const stream = useFakeStream()
 
-  if (!stream) {
-    return null
-  }
-
   return (
     <Room.View
       state={{
         ...initialState,
         terminating,
         stream: RemoteData.Succeed(stream)
+      }}
+      dispatch={action('dispatch')}
+    />
+  )
+}
+
+export const WithRawInfoMessage: React.FC = () => {
+  const messageContent = text('Message Content', 'Raw no JSON message')
+  const stream = useFakeStream()
+
+  return (
+    <Room.View
+      state={{
+        ...initialState,
+        stream: RemoteData.Succeed(stream),
+        outgoingInfoMessages: [{ id: 0, content: messageContent }]
+      }}
+      dispatch={action('dispatch')}
+    />
+  )
+}
+
+export const WithJsonInfoMessage: React.FC = () => {
+  const messageContent = '{"foo":false,"baz":123}'
+
+  const stream = useFakeStream()
+
+  return (
+    <Room.View
+      state={{
+        ...initialState,
+        stream: RemoteData.Succeed(stream),
+        outgoingInfoMessages: [{ id: 0, content: messageContent }]
+      }}
+      dispatch={action('dispatch')}
+    />
+  )
+}
+
+export const WithMultiplyInfoMessages: React.FC = () => {
+  const messagesCount = number('# Messages', 5, {
+    range: true,
+    min: 0,
+    max: 10
+  })
+  const messageContent = text(
+    'Message Content',
+    'Some content of #{id} message'
+  )
+  const stream = useFakeStream()
+
+  return (
+    <Room.View
+      state={{
+        ...initialState,
+        stream: RemoteData.Succeed(stream),
+        outgoingInfoMessages: Array.from({
+          length: messagesCount
+        }).map((_, id) => ({
+          id,
+          content: messageContent.replace(/\{id\}/g, id.toString())
+        }))
       }}
       dispatch={action('dispatch')}
     />
