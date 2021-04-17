3118e52df9c96c20494ed6dda5229598c0a1e102
ovoshlook <ovoshlook@gmail.com>
2021-04-17 23:02:23 +0200

    destroy dlg if connection dropped

--- signalling/server/engine/roles/dialog.lua
+++ signalling/server/engine/roles/dialog.lua
@@ -4,29 +4,25 @@ local jsonrpc = require "tools.jsonrpc"
 
 local function create(fuser,tuser,callid,ftag,ttag)
     local id = base64.encode(json.encode({callid,ftag,ttag}))
-    local dlgusers = base64.encode(json.encode({fuser,tuser}))
     KSR.pv.sets("$sht(dialogs=>username:"..fuser..")",id)
     KSR.pv.sets("$sht(dialogs=>username:"..tuser..")",id)
-    KSR.pv.sets("$sht(dialogs=>id:"..id,dlgusers)
 
 end
 
 local function destroy(user) 
     
-    local id = KSR.pv.get("$sht(dialogs=>username:"..user..")")
+    local u = KSR.pv.get("$sht(dialogs=>username:"..user..")")
 
-    
     if not u then
         KSR.warn("Can't find dialog for with the user "..user.." for removing\n")
         return false
     end
 
     KSR.pv.seti("$shtex(dialogs=>username:"..user..")",1)
-    KSR.pv.sets("$sht(dialogs=>id:"..id,1)
     
-    local uDecoded = json.decode(base64.decode(u))
+    local uDecoded = json.decode(base64.decode(id))
 
-    KSR.jsonrpcs.exec(jsonrpc.requestBody("dlg","terminate_dlg",uDecoded))
+    KSR.jsonrpcs.exec(jsonrpc.requestBody("dlg","terminate_dlg",u))
 
     return true
 
--- signalling/server/engine/sockets/websocket.lua
+++ signalling/server/engine/sockets/websocket.lua
@@ -1,10 +1,15 @@
+local dialogs = require("roles.dialogs")
+
 local function closed()
 
     local ip = KSR.pv.get("$si")
     local port = KSR.pv.get("$sp")
     local conid = KSR.pv.get("$conid")
     KSR.info("Socket closed for: "..ip..":"..port.."\n");
-
+    local user = KSR.pv.get("$sht(users=>lookup:"..conid..")")
+    if user then
+        dialogs.destroy(user)
+    end
 end
 
 return {
